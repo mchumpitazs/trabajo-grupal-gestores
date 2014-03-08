6241203d60345c3a24ae1c8d6b6016f52f561886
Lino Espinoza <woztheman@gmail.com>
2014-03-07 20:40:43 -0500

    Agregado archivos para trabajar con Eclipse, dejando de lado Netbeans

---
+++ src/gestores/modelo/Reunion.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestores.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Lino Espinoza
+ */
+public class Reunion {
+    
+    private int idReunion;
+    private Date fechaReunion;
+    private String observacion;
+    private String calificacion;
+    
+    // Una reunion pertenece a una idea
+    private Idea idea;
+
+    // Constructor
+    
+    // Getters / Setters
+    
+    public int getIdReunion() {
+        return idReunion;
+    }
+
+    public void setIdReunion(int idReunion) {
+        this.idReunion = idReunion;
+    }
+
+    public Date getFechaReunion() {
+        return fechaReunion;
+    }
+
+    public void setFechaReunion(Date fechaReunion) {
+        this.fechaReunion = fechaReunion;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public String getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(String calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Idea getIdea() {
+        return idea;
+    }
+
+    public void setIdea(Idea idea) {
+        this.idea = idea;
+    }
+     
+}
---
+++ src/gestores/modelo/TipoDocumento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestores.modelo;
+
+/**
+ *
+ * @author Lino Espinoza
+ */
+public class TipoDocumento {
+    
+    private int idTipoDocumento;
+    private String descripcion;
+    
+    // TODO: Relacion Clase Usuario
+}
